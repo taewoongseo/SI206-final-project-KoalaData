7e8f05716e3996c16ab5721ee625554016b51acd
Natalie Sangkagalo <nsang@umich.edu>
2020-04-10 04:05:44 -0400

    did some calculations

--- FinalProjectDumps.py
+++ FinalProjectDumps.py
@@ -12,8 +12,44 @@ cur = conn.cursor()
 cur.execute('SELECT Google.name, Yelp.yelp_review_count, Google.google_review_count, Yelp.city FROM Yelp INNER JOIN Google ON Yelp.restaurant_id = Google.restaurant_id')
 conn.commit()
 
+#Calculation time
+tot_yelp = 0
+tot_google = 0
 #Now for looping the joined table!
 rows = cur.fetchall()
 for row in rows:
-    print(row)
+    tot_yelp += row[1]
+    tot_google += row[2]
+print(tot_yelp)
+print(tot_google)
+
+#how much is Google bigger than Yelp?
+difference = (tot_google/tot_yelp)*100
+print(str(difference) + '%')
+
+
+
+#Now... by cities!
+yelp_d = {}
+google_d = {}
+
+for row in rows:
+    city = row[-1]
+    if city not in yelp_d.keys():
+        yelp_d[city] = row[1]
+    else:
+        yelp_d[city] += row[1]
+
+for row in rows:
+    city = row[-1]
+    if city not in google_d.keys():
+        google_d[city] = row[2]
+    else:
+        google_d[city] += row[2]
+
+print(yelp_d)
+print(google_d)
+        
+
+
 
