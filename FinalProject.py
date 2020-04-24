4e44609adb2863160823bf5e558d0beac0e85d7b
Taewoong Seo <taewoongseo@Taewoongs-MacBook-Pro.local>
2020-04-24 14:12:38 +0900

    error fixed

--- FinalProject.py
+++ FinalProject.py
@@ -57,28 +57,32 @@ def gather_data(city_name):
     for i in range(len(biz_names)):
         restaurant_name_for_google = biz_names[i]
         coord_tup_google = biz_coordinates[i]
-        parameters2 = {'key': "AIzaSyAZb7lF87OYuosZWGdDMvNnVaGxazfadXk", "input": restaurant_name_for_google, "inputtype": "textquery", 'locationbias': 'circle:1@' + coord_tup_google, 'fields': "name,user_ratings_total,geometry,formatted_address"}
-        google_r = requests.get(url = endpoint2, params = parameters2)
-        google_data = google_r.json()
-        google_dict.append(google_data)
+        parameters2 = {'key': "AIzaSyAZb7lF87OYuosZWGdDMvNnVaGxazfadXk", "input": restaurant_name_for_google, "inputtype": "textquery", 'locationbias': 'circle:2@' + coord_tup_google, 'fields': "name,user_ratings_total,geometry"}
+        try:
+            google_r = requests.get(url = endpoint2, params = parameters2)
+            google_data = google_r.json()
+            google_dict.append(google_data)
+        except:
+            google_data_null = {'null': 0}
+            google_dict.append(google_data_null)
 
     google_names = []
     google_review_counts = []
-    google_coordinates = []
 
-    try:
+    #Add google_review_counts to a list as long as there are 20 restaurants pulled initially
+    if len(google_dict) == 20:
         for google_biz in google_dict:
-            google_names.append(google_biz['candidates'][0]['name'])
-            if 'user_ratings_total' in google_biz['candidates'][0].keys():
-                google_review_counts.append(google_biz['candidates'][0]['user_ratings_total'])
-            else:
-                google_review_counts.append(0)
-            g_latitude = google_biz['candidates'][0]['geometry']['location']['lat']
-            g_longitude = google_biz['candidates'][0]['geometry']['location']['lng']
-            g_coord_tup = str(g_latitude) + ',' + str(g_longitude)
-            google_coordinates.append(g_coord_tup)
-    except:
-        print("Sorry, some restaurants in the city are not matching Google's results :( Try another city!")
+            try:
+                google_names.append(google_biz['candidates'][0]['name'])
+                if 'user_ratings_total' in google_biz['candidates'][0].keys():
+                    google_review_counts.append(google_biz['candidates'][0]['user_ratings_total'])
+                else:
+                    google_review_counts.append(0)
+            except:
+                print("Sorry, some restaurants in the city are not matching Google's results :( Try another city!")
+                exit()
+    else:
+        exit()
 
     #restaurant_id set up 
     conn = sqlite3.connect('restaurant_data.db')
@@ -92,10 +96,10 @@ def gather_data(city_name):
 
 
     #Set up a database for Yelp
-    cur.execute('CREATE TABLE IF NOT EXISTS Yelp (restaurant_id INTEGER, name TEXT, city TEXT, rating REAL, yelp_review_count INTEGER, coordinates TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Yelp (restaurant_id INTEGER, name TEXT, city TEXT, yelp_review_count INTEGER)')
 
     for i in range(len(biz_names)):
-        cur.execute('INSERT OR IGNORE INTO Yelp (restaurant_id, name, city, rating, yelp_review_count, coordinates) VALUES (?, ?, ?, ?, ?, ?)', (yelp_restaurant_id, biz_names[i], city_name, biz_ratings[i], biz_review_counts[i], biz_coordinates[i]))
+        cur.execute('INSERT OR IGNORE INTO Yelp (restaurant_id, name, city, yelp_review_count) VALUES (?, ?, ?, ?)', (yelp_restaurant_id, biz_names[i], city_name, biz_review_counts[i]))
         yelp_restaurant_id += 1
     conn.commit()
 
@@ -109,12 +113,17 @@ def gather_data(city_name):
     #Set up a database for Google
     conn = sqlite3.connect('restaurant_data.db')
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS Google (restaurant_id INTEGER, name TEXT, google_review_count INTEGER, coordinates TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS Google (restaurant_id INTEGER, google_review_count INTEGER)')
 
+    if len(google_names) == 20:
+        for i in range(len(google_names)):
+            cur.execute('INSERT OR IGNORE INTO Google (restaurant_id, google_review_count) VALUES (?, ?)', (google_restaurant_id, google_review_counts[i]))
+            google_restaurant_id += 1
+        conn.commit()
+    else:
+        print("Sorry, some restaurants in the city are not matching Google's results :( Try another city!")
+        exit()
 
-    for i in range(len(google_names)):
-        cur.execute('INSERT OR IGNORE INTO Google (restaurant_id, name, google_review_count, coordinates) VALUES (?, ?, ?, ?)', (google_restaurant_id, google_names[i], google_review_counts[i], google_coordinates[i]))
-        google_restaurant_id += 1
-    conn.commit()
+    print("Your data has been retrieved!")
 
 gather_data(user_input_city)
